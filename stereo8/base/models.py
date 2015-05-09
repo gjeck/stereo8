49652b72ad29cc9f9fb909318f50f54159626d97
gjeck <gregory.jeckell@gmail.com>
2015-05-09 16:00:46 -0700

    Add pyen and pylast deps. Adjust base models.

--- stereo8/base/models.py
+++ stereo8/base/models.py
@@ -1,29 +1,32 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 class Album(models.Model):
     artist = models.ForeignKey('Artist', blank=True, null=True)
-    label = models.ForeignKey('Label', blank=True, null=True)
     image = models.ForeignKey('Image', blank=True, null=True)
-    mbid = models.CharField(max_length=255, unique=True)
+    label = models.ForeignKey('Label', blank=True, null=True)
+    tags = TaggableManager()
     date = models.DateField()
+    mbid = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    summary = models.TextField()
     score = models.IntegerField()
     score_url = models.URLField()
+    summary = models.TextField()
     def __str__(self):
-        album = (self.name, self.artist.name if self.artist else '(null)')
+        album = (self.name, getattr(self.artist, 'name', '(None)'))
         return '{0} - {1}'.format(*album)
 
 class Artist(models.Model):
+    bio = models.TextField()
     mbid = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    bio = models.TextField()
     def __str__(self):
         return self.name
 
 class Image(models.Model):
-    small = models.URLField()
     large = models.URLField()
+    mbid = models.CharField(max_length=255, unique=True)
+    small = models.URLField()
 
 class Label(models.Model):
     mbid = models.CharField(max_length=255, unique=True)
@@ -32,22 +35,31 @@ class Label(models.Model):
         return self.name
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+    url = models.URLField()
     def __str__(self):
         return self.name
 
 class Review(models.Model):
     album = models.ForeignKey('Album', blank=True, null=True)
     publisher = models.ForeignKey('Publisher', blank=True, null=True)
-    summary = models.TextField()
-    score = models.IntegerField()
     date = models.DateField()
+    score = models.IntegerField()
+    summary = models.TextField()
+    def __str__(self):
+        review = (
+            getattr(self.publisher, 'name', '(None)'),
+            getattr(self.album, 'name', '(None)'),
+            self.score
+        )
+        return '{0} - {1} - {2}'.format(*review)
 
 class Track(models.Model):
-    artist = models.ForeignKey('Artist', blank=True, null=True)
     album = models.ForeignKey('Album', blank=True, null=True)
+    duration = models.IntegerField()
     mbid = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    duration = models.IntegerField()
     def __str__(self):
-        return self.name
+        track = (getattr(self.album, 'name', '(None'), self.name)
+        return '{0} - {1}'.format(*track)
+
